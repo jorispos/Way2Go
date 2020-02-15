a21cb9785c3d6d8dfe074c72786587d877b06ed1
jorispos <jorispostmus@gmail.com>
2020-02-15 13:27:50 -0500

    op

--- src/Window.java
+++ src/Window.java
@@ -8,7 +8,7 @@ public class Window {
 	JPanel titlePanel;
 	JFrame window = new JFrame("Way2Go");
 	Color backgroundColor = new Color(35, 35, 0);
-	Container con;
+	Container cons;
 	
 	public Window() {
 		
@@ -22,12 +22,12 @@ public class Window {
 		window.setLayout(null);
 		window.setLocationRelativeTo(null);
 		window.setResizable(false);
-		con = window.getContentPane();
+		cons = window.getContentPane();
 		
 		titlePanel = new JPanel();
 		titlePanel.setBounds(0, 0, 860, 100);
 		titlePanel.setBackground(backgroundColor);
-		con.add(titlePanel);
+		cons.add(titlePanel);
 	}
 	
 }
